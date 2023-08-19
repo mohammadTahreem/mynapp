927736e2e12580ef24689441ea7b8a0364762281
tahreem <mohammad@tahreem.com>
2023-08-19 08:48:44 +0530

    Initial Commit

---
+++ Myn App/Myn_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Myn_AppApp.swift
+//  Myn App
+//
+//  Created by Tahreem Qadri on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Myn_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
