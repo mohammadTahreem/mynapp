e3d72e39eb37c2feef660a78156821c4b56ef67e
mohammadTahreem <tahreem.qadri@outlook.com>
2023-11-11 10:03:41 +0530

    New haptic feedback added

---
+++ Myn App/HapticFeedbackView.swift
@@ -0,0 +1,18 @@
+//
+//  HapticFeedbackView.swift
+//  Myn App
+//
+//  Created by Tahreem Qadri on 11/11/23.
+//
+
+import SwiftUI
+
+struct HapticFeedbackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticFeedbackView()
+}
---
+++ Myn App/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Myn App
+//
+//  Created by Tahreem Qadri on 19/08/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
