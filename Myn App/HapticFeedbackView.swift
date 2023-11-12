ce3146c3f5dc19b6d579ac3f3c22bebac9eb9818
mohammadTahreem <tahreem.qadri@outlook.com>
2023-11-12 20:26:49 +0530

    Update HapticFeedbackView.swift

--- Myn App/HapticFeedbackView.swift
+++ Myn App/HapticFeedbackView.swift
@@ -10,9 +10,24 @@ import SwiftUI
 struct HapticFeedbackView: View {
     @State var counter = 0
     var body: some View {
-        Button("Counter value is: \(counter)"){
-            counter += 1
-        }.sensoryFeedback(.warning, trigger: counter)
+        VStack{
+            Spacer()
+            Text("The counter count is \(counter)").font(.largeTitle)
+                .foregroundStyle(.white)
+            
+            
+            HStack{
+                Spacer()
+                Button(action: {counter += 1}, label: {
+                    Text("Increase counter").foregroundStyle(.white).padding()
+                })
+                .sensoryFeedback(.warning, trigger: counter)
+                .background(RoundedRectangle(cornerRadius: 25.0).foregroundColor(.green))
+                Spacer()
+            }
+            Spacer()
+        }.background(.purple)
+            .ignoresSafeArea()
     }
 }
 
